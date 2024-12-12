9a6bb5760da8cdc2b3106ee8ea7f6efee9f2bf90
Juan Pablo <jpablo.valdebenitom@gmail.com>
2024-12-12 20:58:34 -0300

    Agregar detalle curso pages

--- src/app/detalle-curso/detalle-curso.page.ts
+++ src/app/detalle-curso/detalle-curso.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConsumoApiService } from '../service/consumo-api.service';
+import * as qrcode from 'qrcode-generator';
+
 
 @Component({
   selector: 'app-detalle-curso',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleCursoPage implements OnInit {
 
-  constructor() { }
+  idProfesor: number = 0
+  idCurso : number = 0
+  nombreCurso : string = ""
+  codigoCurso : string = ""
+  seccionCurso : string = ""
+  qrDataURL : string = ""
+
+  alumnos : any[] = [];
+
+  constructor(private consumoApi: ConsumoApiService, private activateroute: ActivatedRoute, private router: Router) { 
+    this.activateroute.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation()?.extras.state) {
+        this.nombreCurso = this.router.getCurrentNavigation()?.extras.state?.['nombre'];
+        this.idCurso = this.router.getCurrentNavigation()?.extras.state?.['id'];
+        this.codigoCurso = this.router.getCurrentNavigation()?.extras.state?.['codigo'];
+        this.seccionCurso = this.router.getCurrentNavigation()?.extras.state?.['seccion'];
+        this.idProfesor = this.router.getCurrentNavigation()?.extras.state?.['idProfesor'];
+
+        console.log('ID Curso:', this.idCurso);
+        console.log('ID Profesor:', this.idProfesor); // Cambia el valor del profesor según sea necesario
+      }
+    });
+  }
+
+  mostrarAlumnos(profesorId: number, cursoId: number){
+    this.consumoApi.obtenerAlumnosCurso(profesorId, cursoId).subscribe(
+      (respuesta) => {
+        this.alumnos = respuesta;
+      },
+      (error) => {
+        console.error('Error al obtener alumnos: ', error)
+      }
+    );
+  }
+
+  generateQRCode(){
+    if(this.idCurso) {
+      const fechaActual = new Date().toISOString().split('T')[0]; //Fecha en formato YYYY-MM-DD
+      const data = `${this.codigoCurso}-${this.seccionCurso}-${fechaActual}`;
+
+      let qr = qrcode(4, 'L');
+      qr.addData(data);
+      qr.make();
+      this.qrDataURL = qr.createDataURL(4);
+
+    }
+  }
 
   ngOnInit() {
+    this.mostrarAlumnos(this.idProfesor, this.idCurso);
+    this.generateQRCode();
   }
 
 }
