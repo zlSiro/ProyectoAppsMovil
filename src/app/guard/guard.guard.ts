c9a2a7ac8f3c4ff37a8083a825556c85099be087
Juan Pablo <jpablo.valdebenitom@gmail.com>
2024-12-12 20:08:49 -0300

    actualizacion guard

--- src/app/guard/guard.guard.ts
+++ src/app/guard/guard.guard.ts
@@ -4,8 +4,8 @@ import { AuthServiceService } from '../service/auth-service.service';
 import { Router } from '@angular/router';
 
 export const guardGuard: CanActivateFn = (route, state) => {
-  const = authService = inject(AuthServiceService);
-  const = router = inject(Router)
+  const authService = inject(AuthServiceService);
+  const router = inject(Router)
 
   if(authService.isLoggedIn()){
     return true;
