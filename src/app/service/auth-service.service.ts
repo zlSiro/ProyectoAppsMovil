8d42235293833a947395aa4e5056227d521f5ab7
Juan Pablo <jpablo.valdebenitom@gmail.com>
2024-12-12 20:09:11 -0300

    implementacion del AuthService

--- src/app/service/auth-service.service.ts
+++ src/app/service/auth-service.service.ts
@@ -4,6 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthServiceService {
+  // se crea con ionic g service/authservice
+
+  private authenticated = false;
 
   constructor() { }
+
+  isLoggedIn(){
+    return this.authenticated; // Estado que retornara la clase
+  }
+
+  login(){
+    this.authenticated = true; // Cambia estado si el login es exisotoso
+  }
+
+  logout(){
+    this.authenticated = false; // cambia estado para salir de la app
+  }
 }
