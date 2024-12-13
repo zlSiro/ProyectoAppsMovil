6b6606b05595ffaa9773977319984c71b0ad80a3
Juan Pablo <jpablo.valdebenitom@gmail.com>
2024-12-12 22:04:18 -0300

    Agregar alumno pages

--- src/app/alumno/alumno.page.ts
+++ src/app/alumno/alumno.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alumno',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlumnoPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  //Metodo que permite navergar como si fuera un HREF
+  navegar(){
+    this.router.navigate(['/login']);
+  }
 
   ngOnInit() {
+    this.navegar()
   }
 
 }
